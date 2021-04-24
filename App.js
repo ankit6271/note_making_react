961fe094226ec3eb2c74455c010cb0b592dc4824
ankit6271 <ankitbhardwaj6707@gmail.com>
2021-04-24 11:33:48 +0530

    App Changes

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [changeText, setchangeText] = useState('');
+  const [notes,setNotes] =useState([]);
+  const onChangeText = (value) => (
+    setchangeText(value)
+  )
+  const addNoteHandler =() =>(
+    setNotes([...notes,changeText])
+  )
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View >
+        <Text>Note Making App</Text>
+      </View>
+      <View >
+        <TextInput placeholder="Enter a Note" onChangeText={onChangeText}></TextInput>
+        <Button onPress={addNoteHandler} title="Add Note" ></Button>
+      </View>
+      <View>
+        <Text>{notes}</Text>
+      </View>
     </View>
   );
 }
@@ -18,4 +34,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  chidView:{
+
+  }
 });
