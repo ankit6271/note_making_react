ebb4b0d9b7f12adfc911979e3fa16a1b0b9e0f06
ankit6271 <ankitbhardwaj6707@gmail.com>
2021-04-24 17:35:20 +0530

    Ui Changes

--- App.js
+++ App.js
@@ -1,28 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
-  const [changeText, setchangeText] = useState('');
-  const [notes,setNotes] =useState([]);
-  const onChangeText = (value) => (
-    setchangeText(value)
-  )
-  const addNoteHandler =() =>(
-    setNotes([...notes,changeText])
-  )
+  // const [changeText, setchangeText] = useState("");
+  // const [notes,setNotes] =useState([]);
+  // const onChangeText = (value) => (
+  //   setchangeText(value)
+  // )
+  // const addNoteHandler =() =>(
+  //   setNotes([...notes,changeText])
+  // )
   return (
     <View style={styles.container}>
       <View >
         <Text>Note Making App</Text>
       </View>
-      <View >
+      {/* <View >
         <TextInput placeholder="Enter a Note" onChangeText={onChangeText}></TextInput>
         <Button onPress={addNoteHandler} title="Add Note" ></Button>
       </View>
       <View>
         <Text>{notes}</Text>
-      </View>
+      </View> */}
     </View>
   );
 }
